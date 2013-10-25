c232dda52d6dd47be12253eb3c9ac2ff4e95889f
jmptable <jmptable@mit.edu>
2013-10-25 14:22:13 -0400

    Jade hello world.

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require("express");
 var app = express();
 
 var port = 3700;
+
+// jade settings
+app.set('views', __dirname + '/templates');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+	res.render("index");
+});
  
 app.get("/", function(req, res){
 	res.send("It works!");
