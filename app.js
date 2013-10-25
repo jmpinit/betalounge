9a29a07bbe32efcb295155600f7b4bb1710b02ae
jmptable <jmptable@mit.edu>
2013-10-25 14:17:44 -0400

    Hello world express server.

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+var port = 3700;
+ 
+app.get("/", function(req, res){
+	res.send("It works!");
+});
+ 
+app.listen(port);
+console.log("Listening on port " + port);
